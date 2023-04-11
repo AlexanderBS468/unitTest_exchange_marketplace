26d0594914de4aac58a563d67ca82af47fb35bc6
AlexBS <alex.sad.468@gmail.com>
2023-04-11 03:22:31 +0100

    tests for old version bitrix version 19

--- boot.php
+++ boot.php
@@ -10,4 +10,25 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_be
 require_once __DIR__ . '/../vendor/autoload.php';
 
 Main\Loader::includeModule('avito.export');
-Main\Loader::registerNamespace('Tests\Unit', __DIR__ . '/unit');
+
+spl_autoload_register(static function($className) {
+	static $namespace = 'Tests\\Unit\\';
+	static $namespaceLength = null;
+
+	if (!isset($namespaceLength))
+	{
+		$namespaceLength = strlen($namespace);
+	}
+
+	if (strpos($className, $namespace) === 0)
+	{
+		$classNameRelative = substr($className, $namespaceLength);
+		$classRelativePath =  str_replace('\\', '/', $classNameRelative) . '.php';
+		$classFullPath = __DIR__ . '/unit/' . $classRelativePath;
+
+		if (file_exists($classFullPath))
+		{
+			require_once $classFullPath;
+		}
+	}
+});
